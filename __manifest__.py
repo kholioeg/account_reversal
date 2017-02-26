b27231e98f9f3b7c075ea9fbd5162b33cc627331
Khaled Hamedq <kholioeg@gmail.com>
2017-02-26 02:53:54 +0200

    Initial Commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Account Reversal (V10c)",
+    "name": "Account Reversal (V10C)",
     "summary": "Wizard for creating a reversal account move",
     "category": "Accounting & Finance",
     "website": "https://odoo-community.org/",
@@ -8,10 +8,12 @@
               "Camptocamp,"
               "ACSONE SA/NV,"
               "Tecnativa,"
+              "Khaled Hamed"
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
+    'version': '10.0.1.0',
     # any module necessary for this one to work correctly
     'depends': ['account'],
 
